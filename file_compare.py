9f40e9ac8325e0706d8fd82504645ce5dd20cfd5
Sairam Aripirala <sairama@aweber.com>
2020-08-20 02:09:47 -0400

    separate dir_read and exception testcase

--- file_compare.py
+++ file_compare.py
@@ -5,25 +5,24 @@ from os.path import getsize, join
 LOGGER = logging.getLogger(__name__)
 
 
-def compare():
+def read_dir(path):
     try:
         for root, dirs, files in os.walk('data/from/compare'):
             LOGGER.debug(root, 'consumes', end=' ')
             LOGGER.debug(
                 sum(getsize(join(root, name)) for name in files), end=' ')
             LOGGER.debug('bytes in', len(files), 'non-directory files')
+        return files
     except OSError as e:
-        LOGGER.error('Exception occurred while reading from/compare', e)
+        LOGGER.error('Exception occurred while reading from/compare %s', str(e))
+        raise OSError
 
-    try:
-        for root, dirs, files in os.walk('data/to/compare'):
-            LOGGER.debug(root, 'consumes', end=' ')
-            LOGGER.debug(
-                sum(getsize(join(root, name)) for name in files), end=' ')
-            LOGGER.debug('bytes in', len(files), 'non-directory files')
-    except OSError as e:
-        LOGGER.error('Exception occurred while reading to/compare', e)
 
+def compare():
+    from_compare = read_dir('data/from/compare')
+    to_compare = read_dir('data/to/compare')
+    LOGGER.debug('list of files %s', from_compare)
+    LOGGER.debug('list of files %s', to_compare)
     return True
 
 
--- tests.py
+++ tests.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest import mock
 
 import file_compare
 
@@ -6,3 +7,15 @@ import file_compare
 class TestDataCompare(unittest.TestCase):
     def test_directory_read(self):
         self.assertEqual(file_compare.compare(), True)
+
+    @mock.patch('os.walk')
+    def test_read_dir_exception(self, os_walk):
+        os_walk.side_effect = OSError
+        with self.assertRaises(OSError):
+            file_compare.compare()
+
+    @mock.patch('os.walk')
+    def test_read_dir_exception(self, os_walk):
+        os_walk.side_effect = OSError
+        with self.assertRaises(OSError):
+            file_compare.compare()
